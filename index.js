f8f3bddd66554f3469d987cc4d1acf70516f6a04
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2022-02-09 08:55:51 +0100

    dog fetch implemented

--- index.js
+++ index.js
@@ -20,3 +20,68 @@ if ("serviceWorker" in navigator) {
   /* do non pwa stuff here*/
 }
 
+
+/* fetch the dog*/
+
+let myFetchedData=loadData('https://dog.ceo/api/breeds/image/random');
+
+
+
+//Load Data function
+function loadData(apiUrl){
+
+    console.log("fetching data");
+   
+
+    fetch(apiUrl)
+
+    .then(   (response) => {
+      // wait for fetch to complete
+
+
+        return response.json();
+
+
+
+
+
+      }
+      )
+      
+      .then(    (data) => {
+        // do something with 'data' 
+
+        buildView(data.message);
+      
+      })
+
+      .catch(
+
+      );
+}
+
+
+
+
+
+// dom setup
+// get the Dom element to work within
+let app=document.getElementById("doggo");
+
+
+function buildView(myImgUrl){
+
+
+  let myTitle=document.createElement("h2");
+
+  myTitle.innerText="En Hund";
+   
+let dogImg=document.createElement("img");
+dogImg.src=myImgUrl;
+
+app.appendChild(myTitle);
+app.appendChild(dogImg);
+
+}
+
+
