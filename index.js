7bc30a02511990f357279f02f84590a1e2be476c
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2022-02-09 10:48:50 +0100

    static cache

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ if ("serviceWorker" in navigator) {
   
   .then(registration => {
     console.log("sw registered");
-    console.log(registration);
+    loadData('https://dog.ceo/api/breeds/image/random');
 
   }).catch(error => {
     console.log("sw reg fail");
     console.log(error);
+
   })
 
 } else {
@@ -23,7 +24,7 @@ if ("serviceWorker" in navigator) {
 
 /* fetch the dog*/
 
-let myFetchedData=loadData('https://dog.ceo/api/breeds/image/random');
+//let myFetchedData=loadData('https://dog.ceo/api/breeds/image/random');
 
 
 
--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-let CACHE_NAME = 'my-site-cache';
+const CACHE_NAME = 'my-site-cache';
+const Dynamic_cache = 'site-dynamic-cachee';
 
 let urlsToCache = [
   
@@ -19,17 +20,87 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
 
       .then(function(cache) {
-        console.log('Opened cache');
+        console.log('initial cache of statics');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', function(e){
 
-  console.log('intercept req: '+e.request.url);
+
+
+
+
+
+self.addEventListener('fetch', function(event){
+
+  console.log('URL: '+event.request.url);
+  
+  if (event.request.url.includes('img') ) {
+    console.log('found img: '+event.request.url);
+  }
+
+  if (event.request.url.includes('css') ) {
+    console.log('found css: '+event.request.url);
+    cacheOnly(event);
+  }
+
+  if (event.request.url.includes('images.dog.ceo') ) {
+    /* found a dog cache it*/  
+    console.log('found dog img to cache: '+event.request.url);
+  }
 
   // caching strategies goes here!!
 
 
 });
+
+function cacheOnly(event){
+  console.log('cacheOnly: '+event.request.url);
+  event.respondWith(caches.match(event.request));
+  
+
+}
+
+
+/* ------------------ cache first  -----------------*/
+/*
+self.addEventListener('fetch', function(event) {
+ 
+  event.respondWith(
+    caches.open(CACHE_NAME)
+
+    .then(function(cache) {
+      let result=cache.match(event.request)
+      console.log('cache match: '+result)
+
+      return result
+
+      .then(function (response) {
+
+        return response || fetch(event.request).then(function(response) {
+
+          cache.put(event.request, response.clone());
+          console.log('to cache '+response);
+          return response;
+        });
+      });
+    })
+  );
+});
+*/
+
+
+/* network med fallback til cache 
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+
+      cache.put(event.request);
+      return caches.match(event.request);
+    })
+  );
+});*/
+
+
